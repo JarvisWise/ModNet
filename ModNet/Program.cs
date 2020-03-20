a766d1db2e3df0c614e5c6e3e01f51d008b85bf0
JarvisWise <mrkolisnyk13@gmail.com>
2020-03-20 13:15:21 +0200

    Добавьте файлы проекта.

---
+++ ModNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
